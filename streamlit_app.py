ee51db94a06b477881cda83ecb402e8e4c52e392
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-20 18:05:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ import pandas as pd
 import streamlit as st
 
 st.title('PyDiction')
+st.title('I- Contexte et Objectifs')
+
+st.text("Ce projet de machine learning et de météorologie s'est déroulé dans le cadre de notre formation chez DataScientest.")
+st.text("L'outil permet de prédire la présence de pluie (il  est considéré 'il pleut' si la quantité de pluie est strictement supérieure à 1mm), sur n’importe quel point géographique du territoire australien à partir des données précédemment citées')
+dataset_uploaded = st.file_uploader('charger les données ')
