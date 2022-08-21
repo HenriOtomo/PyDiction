27d965c306e4b76209c8689f831a5ee881b6bf94
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-21 18:18:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 #introduction pour l'utilisateur et chargement des données
 st.title('PyDiction')
-st.title("Présentation de l' application et chargement des données :")
+st.title("Présentation de et chargement des données :")
 st.text("Ce projet de machine learning et de météorologie s'est déroulé dans le cadre de notre formation chez DataScientest.")
 st.text("L'outil permet de prédire la présence de pluie à J+1 sur n importe quel point géographique du territoire australien à partir des données climatiques, il  est considéré qu il pleut si la quantité de pluie est strictement supérieure à 1mm")
 st.text("chargez d'abord vos données, elles doivent être de la meme forme que ce dataset https://www.kaggle.com/jsphyg/weather-dataset-rattle-package sur lequel a été entrainé notre modèle")        
